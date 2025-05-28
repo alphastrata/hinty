91706f351a4a761e579cd56bf1bf6b9d383f8453
Jer <alphastrata@gmail.com>
2025-05-28 22:22:16 +1000

    a different approach
    wiki strings, a hot loop.

--- README.md
+++ README.md
@@ -4,11 +4,10 @@ See the [blog post](https://jeremyfwebb.ninja/2025/2025-MAY-20) for more info.
 
 I was trying to answer the question, _does `std::hint::likely()` or `unlikely()` **really** do anything?_
 
-```sh
-cargo build --release
-./gen_data.sh 10000000 # 10 million
-cargo bench
-./run_perf.sh
-```
 
- 
+#data:
+```sh
+mkdir datasets
+wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-all-titles-in-ns0.gz -O datasets/enwiki-latest-all-titles-in-ns0.gz
+gunzip datasets/enwiki-latest-all-titles-in-ns0.gz
+ ```
---
+++ bins/likely.rs
@@ -0,0 +1,30 @@
+#![allow(internal_features)]
+#![feature(core_intrinsics)]
+use hinty::wikimedia_dataset_as_vec;
+use std::env;
+
+fn main() {
+    let queries = env::args().collect::<Vec<String>>();
+
+    let data = wikimedia_dataset_as_vec();
+    let mut misses = 0;
+
+    println!("Entries: {}", data.len());
+
+    for q in queries.iter().skip(1) {
+        println!("Searching... {q}");
+        
+        for title in data.iter() {
+            if std::intrinsics::likely(*title == q.to_lowercase()) {
+            if *title == q.to_lowercase() {
+                println!("{q} was located");
+                break;
+            }
+            else {
+                misses += 1;
+            }
+        }
+    }
+
+    println!("Misses : {}", misses);
+}
